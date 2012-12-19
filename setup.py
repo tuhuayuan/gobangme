35cf5fe28eac6a488f7c0df641b5886457a77b5a
tuhuayuan <tuhuayuan@gmail.com>
2012-12-19 14:40:42 +0800

    初始化目录机构

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = 'gobang',
+    version='1.0',
+    description=u"朋友间的优惠分享",
+    author="tuhuayuan",
+    author_email='tuhuayuan@gmail.com',
+    url='',
+    packages=find_packages(),
+    package_data={},
+    scripts=[],
+)
